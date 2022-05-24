e1a85271c4c9c2c5585b311424aab9de32fff088
Manu-Laptop-Lnx <huettel.m@gmail.com>
2022-05-24 11:07:43 +0200

    startet findAll route controller

--- api/app/controllers/shift.controller.js
+++ api/app/controllers/shift.controller.js
@@ -1,7 +1,6 @@
-
-const { handleSuccess, handleInternalError, handleNotFound,handleValidationError } = require('../helpers/response');
+const { handleSuccess, handleInternalError, handleNotFound, handleValidationError } = require('../helpers/response');
 const db = require('../models');
-
+const Shift = db.Shift;
 // Create and Save a new Appointment
 exports.create = (req, res) => {
   const validationResponse = handleValidationError(req, res);
@@ -12,14 +11,52 @@ exports.create = (req, res) => {
   // Save Appointment in the database
 };
 
-// Retrieve all Appointments from the database.
-exports.getRange = (req, res) => {
+// Retrieve all shifts from the database.
+exports.findAll = (req, res) => {
   const validationResponse = handleValidationError(req, res);
   if (validationResponse !== null) {
     return validationResponse;
-
   }
 
+  let reqSurname = req.query.surname;
+
+  if (!reqSurname) {
+    handleInternalError(res, 'missing parameter');
+    moduleLogger.error('no surname in query paramter');
+    return;
+  }
+  let reqId = req.query.id;
+
+  if (!reqId) {
+    handleInternalError(res, 'missing parameter');
+    moduleLogger.error('no id in query paramter');
+    return;
+  }
+  Shift.findAll({
+    where: {
+      surname: reqSurname,
+      id: reqId
+    }
+  })
+    .then(data => {
+      moduleLogger.debug(data);
+      if (data.length == 0) {
+        handleSuccess(res, 'no valid surname', {
+          user: {
+            valid: false
+          }
+        });
+      } else {
+        handleSuccess(res, 'valid surname', {
+          user: {
+            valid: true
+          }
+        });
+      }
+    })
+    .catch(error => {
+      moduleLogger.debug(error);
+    });
 };
 
 // Update a Appointment by the id in the request
@@ -36,5 +73,4 @@ exports.delete = (req, res) => {
   if (validationResponse !== null) {
     return validationResponse;
   }
-
 };
